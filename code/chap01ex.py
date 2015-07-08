12cf12a83e74baf541b929316fa37fa071bec0d4
Matt Mollison <matt.mollison@gmail.com>
2015-07-07 20:07:10 -0700

    working on chapter 1

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -13,6 +13,39 @@ import sys
 import nsfg
 import thinkstats2
 
+def ReadFemResp(dct_file='2002FemResp.dct',
+                dat_file='2002FemResp.dat.gz'):
+    """Reads the NSFG resp data.
+
+    dct_file: string file name
+    dat_file: string file name
+
+    returns: DataFrame
+    """
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip')
+    CleanFemResp(df)
+    return df
+
+def CleanFemResp(df):
+    """Recodes variables from the respondent frame.
+
+    df: DataFrame
+    """
+    pass
+
+def ValidatePregnum(df_resp):
+    # read the pregnancy dataframe
+    preg = nsfg.ReadFemPreg()
+    
+    # dictionary to map caseid to indices of pregnancies in preg df
+    preg_map = nsfg.MakePregMap(preg)
+    
+    for case in df_resp['caseid'].unique():
+        if len(preg_map[case]) != df_resp[df_resp['caseid'] == case].pregnum.values:
+            print(case)
+            return False
+    return True
 
 def main(script):
     """Tests the functions in this module.
